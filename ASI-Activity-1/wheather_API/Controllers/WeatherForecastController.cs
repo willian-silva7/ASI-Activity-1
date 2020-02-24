fcb9fcad286ab985611a6a6f9e496b744a464f1e
Willian de Oliveira Silva <37350277+willian-silva7@users.noreply.github.com>
2020-02-24 19:29:08 -0300

    Modify Get

---
+++ ASI-Activity-1/wheather_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace wheather_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        /*
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        */
+
+        public WeatherForecast Get()
+        {
+            var cidade = new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 21,
+                Summary = "Sunny"
+            };
+
+            return cidade;
+        }
+
+        [HttpPost]
+        public WeatherForecast Post(int zipCode)
+        {
+            var cidade = new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 21,
+                Summary = "Sunny"
+            };
+            return cidade;
+        }
+
+    }
+}
